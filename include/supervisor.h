f2c5c271bfe7653e6f1c79255d684a729809c326
GANDHI JENIL CHANDRESH <2021002386.gcet@cvmu.edu.in>
2026-04-30 17:42:48 +0530

    Updated

--- include/supervisor.h
+++ include/supervisor.h
@@ -10,5 +10,5 @@ bool supervisor_request_transition(ElectionState next);
 bool supervisor_is_valid_transition(ElectionState from, ElectionState to);
 uint32_t supervisor_get_transition_count(void);
 
-void supervisor_force_tamper_lockdown(TamperType type);
+void supervisor_force_tamper_lockdown(void);
 const char* supervisor_state_name(ElectionState s);
